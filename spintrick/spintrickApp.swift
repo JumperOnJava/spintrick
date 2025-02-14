ad03e050d5c7fecd308b1e0c87420cf372a988b2
JumperOnJava <jumpergooog@gmail.com>
2025-02-14 20:56:58 +0200

    Initial Commit

---
+++ spintrick/spintrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  spintrickApp.swift
+//  spintrick
+//
+//  Created by Slywa on 14.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct spintrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
